608103132db391495fcf5b8cf668ac524161f8e9
He Shaohui <2876848345@qq.com>
2025-02-07 17:22:47 +0800

    feature：开发短链接后端管理项目初始化

---
+++ admin/src/main/java/com/saas/shortlink/admin/dto/resp/UserRespDTO.java
@@ -0,0 +1,35 @@
+package com.saas.shortlink.admin.dto.resp;
+
+import java.util.Date;
+
+/**
+ * 用户返回参数实体
+ */
+public class UserRespDTO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+}
