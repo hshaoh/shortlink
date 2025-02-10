19ae7225802c91bf50ca5dec354e3d0f3dbb4bd9
He Shaohui <2876848345@qq.com>
2025-02-10 15:00:32 +0800

    feature: 短链接分组分表处理

--- admin/src/test/java/com/saas/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/saas/shortlink/admin/test/UserTableShardingTest.java
@@ -2,20 +2,19 @@ package com.saas.shortlink.admin.test;
 
 public class UserTableShardingTest {
 
-    public static final String SQL = "CREATE TABLE `t_user_%d`  (\n" +
-            "        `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "        `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',\n" +
-            "        `password` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',\n" +
-            "        `real_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '真实姓名',\n" +
-            "        `phone` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号',\n" +
-            "        `mail` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',\n" +
-            "        `deletion_time` bigint NULL DEFAULT NULL COMMENT '注销时间戳',\n" +
-            "        `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',\n" +
-            "        `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',\n" +
-            "        `del_flag` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识 0: 未删除 1: 已删除',\n" +
-            "        PRIMARY KEY (`id`) USING BTREE,\n" +
-            "        UNIQUE INDEX `idx_username`(`username`) USING BTREE\n" +
-            "        ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;";
+    public static final String SQL = "create table `t_group_%d`\n" +
+            "(\n" +
+            "    `id`          bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "    `gid`         varchar(32)  DEFAULT NULL COMMENT '分组标识',\n" +
+            "    `name`        varchar(64)  DEFAULT NULL COMMENT '分组名称',\n" +
+            "    `username`    varchar(256) DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "    `sort_order`  int(3)        DEFAULT NULL COMMENT '分组排序',\n" +
+            "    `create_time` datetime    DEFAULT NULL COMMENT '创建时间',\n" +
+            "    `update_time` datetime    DEFAULT NULL COMMENT '修改时间',\n" +
+            "    `del_flag`    tinyint(1)   DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "    PRIMARY KEY (`id`),\n" +
+            "    UNIQUE KEY `idx_unique_username_gid` (gid, username) USING BTREE\n" +
+            ")ENGINE=InnoDB AUTO_INCREMENT=1716734146606301186 DEFAULT CHARSET=utf8mb4;";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
